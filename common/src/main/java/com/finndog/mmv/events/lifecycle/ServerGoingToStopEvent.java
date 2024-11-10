d706eef765111b10341ccea1005b777e7a7cb637
FinnSetchell <finn@setchell.org>
2024-11-10 16:00:51 +0100

    configured as moogs missing structures

---
+++ common/src/main/java/com/finndog/mmv/events/lifecycle/ServerGoingToStopEvent.java
@@ -0,0 +1,13 @@
+package com.finndog.mmv.events.lifecycle;
+
+
+import com.finndog.mmv.events.base.EventHandler;
+
+public class ServerGoingToStopEvent {
+
+    public static final ServerGoingToStopEvent INSTANCE = new ServerGoingToStopEvent();
+
+    public static final EventHandler<ServerGoingToStopEvent> EVENT = new EventHandler<>();
+
+    private ServerGoingToStopEvent() {}
+}
--- common/src/main/java/com/finndog/mst/events/lifecycle/TagsUpdatedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.finndog.mst.events.lifecycle;
-
-import com.finndog.mst.events.base.EventHandler;
-import net.minecraft.core.RegistryAccess;
-
-public record TagsUpdatedEvent(RegistryAccess registryAccess, boolean fromPacket) {
-
-    public static final EventHandler<TagsUpdatedEvent> EVENT = new EventHandler<>();
-}
